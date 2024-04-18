054d2e5d8367f7388468cc289f9831dcce008797
Joaquin Rienzi <joaquin.rienzi@alu.inspt.utn.edu.ar>
2024-04-18 16:37:02 -0300

    Calculadora V2.0

--- sumador.c
+++ sumador.c
@@ -3,19 +3,22 @@
 
 int main(void) 
 {
-    int num01, num02, num03, suma;
+    int num01, num02, suma, resta, mult, div;
 
     printf("Ingrese un valor: ");
     scanf("%d", &num01);
     printf("Ingrese otro valor: ");
     scanf("%d", &num02);
-    printf("Ingrese otro valor: ");
-    scanf("%d", &num03);
 
-    suma = num01 + num02 + num03;
+    suma = num01 + num02;
+    resta = num01 - num02;
+    mult = num01 * num02;
+    div = num01/num02;
 
-    printf("%d + %d + %d = %d", num01, num02, num03, suma);
-
-    suma = num01 > num02 ? printf("numero %d mayor", num01) : printf("numero %d mayor", num02);
+    printf("la suma es: %d + %d = %d ",num01,num02,suma);
+    printf("la resta es: %d - %d = %d ",num01,num02,resta);
+    printf("la multiplicacion es: %d * %d = %d ",num01,num02,mult);
+    printf("la division es: %d / %d = %d ",num01,num02,div);
+   
     return 0;
 }
